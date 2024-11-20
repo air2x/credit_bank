b00d4894a75c74336a4e71eedf282b62e450829a
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2024-11-20 18:25:39 +0300

    Initial commit

---
+++ calculator_microservice/src/main/java/ru/neoflex/calculator_microservice/CalculatorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.calculator_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMicroserviceApplication.class, args);
+	}
+
+}
