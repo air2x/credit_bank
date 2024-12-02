e96730c8851691977b7721bec6640bb66dfe4e99
Airat <airat91091@mail.ru>
2024-12-02 07:19:51 +0300

    'fixed ScoringDataDto'

--- calculator_microservice/src/main/java/ru/neoflex/calculator_microservice/dto/ScoringDataDto.java
+++ calculator_microservice/src/main/java/ru/neoflex/calculator_microservice/dto/ScoringDataDto.java
@@ -34,42 +34,33 @@ public class ScoringDataDto {
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Фамилия должна содержать только латинские буквы")
     private String lastName;
 
-    @Size(min = 2, max = 30, message = "Фамилия должна быть не меньше 2 и не больше 30 символов")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Фамилия должна содержать только латинские буквы")
+    @Size(min = 2, max = 30, message = "Отчество должно быть не меньше 2 и не больше 30 символов")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Отчество должно содержать только латинские буквы")
     private String middleName;
-
 
     private Gender gender;
 
-
     private LocalDate birthday;
 
-    @Pattern(regexp = "^\\d{4}$", message = "Номер паспорта состоит из 4 цифр")
+    @Pattern(regexp = "^\\d{4}$", message = "Серия паспорта состоит из 4 цифр")
     private String passportSeries;
 
-    @Pattern(regexp = "^\\d{6}$", message = "Серия паспорта состоит из 6 цифр")
+    @Pattern(regexp = "^\\d{6}$", message = "Номер паспорта состоит из 6 цифр")
     private String passportNumber;
 
     private LocalDate passportIssueDate;
 
-
     private String passportIssueBranch;
-
 
     private MaritalStatus maritalStatus;
 
-
     private Integer dependentAmount;
-
 
     private EmploymentDto employment;
 
-
     private String accountNumber;
 
-
     private Boolean isInsuranceEnabled;
-
 
     private Boolean isSalaryClient;
 }
