5a2ff53f29690ccc6c8bb515b5af289e7d5c4dda
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2024-12-09 17:34:28 +0300

    'add MS-deal'

---
+++ deal_microservice/src/main/java/ru/neoflex/deal_microservice/DealMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.deal_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealMicroserviceApplication.class, args);
+    }
+
+}
