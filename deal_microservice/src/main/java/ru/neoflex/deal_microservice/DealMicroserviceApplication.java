3d14fb4dac2372028ba08fded0d49ac1ecef937a
Airat <airat91091@mail.ru>
2024-12-19 09:42:45 +0300

    'add Feign dependency'

--- deal_microservice/src/main/java/ru/neoflex/deal_microservice/DealMicroserviceApplication.java
+++ deal_microservice/src/main/java/ru/neoflex/deal_microservice/DealMicroserviceApplication.java
@@ -3,9 +3,11 @@ package ru.neoflex.deal_microservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DealMicroserviceApplication {
 
     public static void main(String[] args) {
--- deal_microservice/src/test/java/ru/neoflex/deal_microservice/services/RequestInMSCalcServiceTest.java
+++ deal_microservice/src/test/java/ru/neoflex/deal_microservice/services/RequestInMSCalcServiceTest.java
@@ -1,13 +1,38 @@
 package ru.neoflex.deal_microservice.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestClient;
+import ru.neoflex.dto.FinishRegistrationRequestDto;
+import ru.neoflex.dto.LoanStatementRequestDto;
 
 class RequestInMSCalcServiceTest {
 
+    @InjectMocks
+    private RequestInMSCalcService requestInMSCalcService;
+
+    @Mock
+    private RestClient restClient;
+
+    @Mock
+    private CreditService creditService;
+
+    private LoanStatementRequestDto loanStatementRequestDto;
+    private FinishRegistrationRequestDto finishRegistrationRequestDto;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        loanStatementRequestDto = new LoanStatementRequestDto();
+        finishRegistrationRequestDto = new FinishRegistrationRequestDto();
+    }
+
     @Test
     void getLoanOffers() {
+
     }
 
     @Test
