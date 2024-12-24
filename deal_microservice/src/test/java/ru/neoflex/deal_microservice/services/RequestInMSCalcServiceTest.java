22b4ebb32340ca56f3dbc282ce196f018d06d532
Airat <airat91091@mail.ru>
2024-12-24 10:14:37 +0300

    'add tests'

--- deal_microservice/src/test/java/ru/neoflex/deal_microservice/services/RequestInMSCalcServiceTest.java
+++ deal_microservice/src/test/java/ru/neoflex/deal_microservice/services/RequestInMSCalcServiceTest.java
@@ -1,11 +1,12 @@
 package ru.neoflex.deal_microservice.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.web.client.RestClient;
+import ru.neoflex.deal_microservice.exceptions.MSDealException;
 import ru.neoflex.dto.FinishRegistrationRequestDto;
 import ru.neoflex.dto.LoanStatementRequestDto;
 
@@ -13,9 +14,6 @@ class RequestInMSCalcServiceTest {
 
     @InjectMocks
     private RequestInMSCalcService requestInMSCalcService;
-
-    @Mock
-    private RestClient restClient;
 
     @Mock
     private CreditService creditService;
@@ -36,6 +34,27 @@ class RequestInMSCalcServiceTest {
     }
 
     @Test
+    void getLoanOffersIfLoanStatementRequestDtoIsNull() {
+        Exception ex = Assertions.assertThrows(MSDealException.class, () ->
+                requestInMSCalcService.getLoanOffers(null));
+        Assertions.assertEquals("LoanStatementRequestDto cannot be null", ex.getMessage());
+    }
+
+    @Test
     void calculateFinish() {
+    }
+
+    @Test
+    void calculateFinishIfFinishRegistrationRequestDtoISNull() {
+        Exception ex = Assertions.assertThrows(MSDealException.class, () ->
+                requestInMSCalcService.calculateFinish(null, "123"));
+        Assertions.assertEquals("FinishRegistrationRequestDto cannot be null", ex.getMessage());
+    }
+
+    @Test
+    void calculateFinishIfStatementIdISNull() {
+        Exception ex = Assertions.assertThrows(MSDealException.class, () ->
+                requestInMSCalcService.calculateFinish(finishRegistrationRequestDto, null));
+        Assertions.assertEquals("StatementId cannot be null", ex.getMessage());
     }
 }
