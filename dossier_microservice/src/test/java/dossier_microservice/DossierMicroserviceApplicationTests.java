21c83b284ceb9ee93ee831fa46fe629302ce4c8e
Airat <airat91091@mail.ru>
2025-01-09 12:43:06 +0300

    'create ms dossier'

---
+++ dossier_microservice/src/test/java/dossier_microservice/DossierMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dossier_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
