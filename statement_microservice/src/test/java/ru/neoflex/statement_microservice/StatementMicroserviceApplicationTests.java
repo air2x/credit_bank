ffb88809557cdfed1ded3f0e9c0dd8c2e6afce9e
Airat <airat91091@mail.ru>
2025-01-27 11:30:10 +0300

    'commit'

---
+++ statement_microservice/src/test/java/ru/neoflex/statement_microservice/StatementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.statement_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
